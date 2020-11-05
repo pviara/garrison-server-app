681c1622a954bf02e13ddb3ef2c8fb26800edc24
Pierre Viara <viara@intechinfo.fr>
2020-11-06 00:03:09 +0100

    store(list): 📒 add unit

---
+++ src/store/lists/unit.list.ts
@@ -0,0 +1,204 @@
+import { IUnitList } from '../../config/models/list/unit.list.model';
+
+export const unitList: IUnitList = {
+  id: 5,
+  type: 'UNITS',
+  dataset: [{
+    code: 'peasant',
+    types: 'worker',
+    word: [{
+      side: 'alliance',
+      jargon: 'peasant'
+    },
+    {
+      side: 'horde',
+      jargon: 'peon'
+    }],
+    instantiation: {
+      building: 'town-hall',
+      cost: {
+        gold: 60,
+        wood: 10,
+        food: 1
+      },
+      duration: 45
+    }
+  },
+  {
+    code: 'militia',
+    types: [
+      'explorer',
+      'fighter'
+    ],
+    word: [{
+      side: 'alliance',
+      jargon: 'militia'
+    },
+    {
+      side: 'horde',
+      jargon: 'watcher'
+    }],
+    instantiation: {
+      building: 'town-hall',
+      cost: {
+        gold: 95,
+        wood: 0,
+        food: 2
+      },
+      duration: 60
+    },
+    statistics: {
+      health: 120,
+      attack: {
+        points: {
+          min: 6,
+          max: 11
+        },
+        cooldown: 3,
+        isDistance: false
+      },
+      defense: {
+        points: {
+          min: 1,
+          max: 5
+        },
+        cooldown: 10,
+        isDistance: false
+      }
+    }
+  },
+  {
+    code: 'footman',
+    types: 'fighter',
+    word: [{
+      side: 'alliance',
+      jargon: 'footman'
+    },
+    {
+      side: 'horde',
+      jargon: 'grunt'
+    }],
+    instantiation: {
+      building: 'barracks',
+      cost: {
+        gold: 135,
+        wood: 0,
+        food: 2
+      },
+      duration: 75
+    },
+    statistics: {
+      health: 200,
+      attack: {
+        points: {
+          min: 9,
+          max: 17
+        },
+        cooldown: 5,
+        isDistance: false
+      },
+      defense: {
+        points: {
+          min: 3,
+          max: 7
+        },
+        cooldown: 12,
+        isDistance: false
+      }
+    }
+  },
+  {
+    code: 'rifleman',
+    types: [
+      'explorer',
+      'fighter'
+    ],
+    word: [{
+      side: 'alliance',
+      jargon: 'footman'
+    },
+    {
+      side: 'horde',
+      jargon: 'headhunter'
+    }],
+    instantiation: {
+      building: 'barracks',
+      cost: {
+        gold: 205,
+        wood: 30,
+        food: 3
+      },
+      duration: 85,
+      required: {
+        buildings: {
+          code: 'forge'
+        }
+      }
+    },
+    statistics: {
+      health: 200,
+      attack: {
+        points: {
+          min: 9,
+          max: 16
+        },
+        cooldown: 4,
+        isDistance: true
+      },
+      defense: {
+        points: {
+          min: 2,
+          max: 6
+        },
+        cooldown: 15,
+        isDistance: false
+      }
+    }
+  },
+  {
+    code: 'knight',
+    types: 'fighter',
+    word: [{
+      side: 'alliance',
+      jargon: 'knight'
+    },
+    {
+      side: 'horde',
+      jargon: 'wolfrider'
+    }],
+    instantiation: {
+      building: 'barracks',
+      cost: {
+        gold: 310,
+        wood: 50,
+        food: 4
+      },
+      duration: 115,
+      required: {
+        buildings: {
+          code: 'town-hall',
+          upgradeLevel: 2
+        }
+      }
+    },
+    statistics: {
+      health: 260,
+      attack: {
+        points: {
+          min: 18,
+          max: 25
+        },
+        cooldown: 6,
+        isDistance: false
+      },
+      defense: {
+        points: {
+          min: 5,
+          max: 10
+        },
+        cooldown: 8,
+        isDistance: false
+      }
+    }
+  }]
+}

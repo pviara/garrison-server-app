3eb0f41b2d00d48f0ebfebf34af0838770a37003
Pierre Viara <viara@intechinfo.fr>
2020-11-10 10:44:07 +0100

    feat(model): 🔩 add user

---
+++ src/config/models/user/user.model.ts
@@ -0,0 +1,21 @@
+import { IDocument } from '../global.model';
+
+/**
+ * The representation of a user.
+ */
+export interface IUser extends IDocument {
+  /** User's both displayed nickname and login. */
+  username: string;
+  
+  /** User's both e-mail and login. */
+  email: string;
+
+  /** User's password details. */
+  password: {
+    hash: string;
+    salt: string;
+  };
+
+  /** Indicates whether the user has confirmed his account. */
+  isConfirmed: boolean;
+}

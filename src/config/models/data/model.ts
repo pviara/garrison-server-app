f895a2277433e46297890e799452c6a9840f9f49
Pierre Viara <viara@intechinfo.fr>
2020-11-13 12:39:26 +0100

    refacto(model): 🔩 remove useless models

--- src/config/models/data/model.ts
+++
@@ -1,8 +0,0 @@
-/**
- * A document is a data record from any datastore
- * ("any" because no DBMS has been chosen for now).
- */
-export interface IDocument {
-  /** Unique document identifier. */
-  id: number;
-}

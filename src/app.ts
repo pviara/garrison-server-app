b54b8433eb1256bf92049218c467ef7bf3507952
Pierre Viara <viara@intechinfo.fr>
2020-11-22 11:50:31 +0100

    core(setup): 🚀 update app file consequently

--- src/app.ts
+++ src/app.ts
@@ -1,42 +1,63 @@
 import dotenv from 'dotenv';
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 
-import initService from './config/services/init.service';
+import { initService } from './config/services/init.service';
 
-import dbService from './config/services/database/database.service';
+import ErrorHandler from './config/models/error/error-handler.model';
 
-// load the environment variables from the .env file
-dotenv.config({
-  path: '.env'
-});
+import MasterRouter from './config/routers/master.router';
 
-/**
- * Express server application class.
- * @description Will later contain the routing system.
- */
-class Server {
-  public app = express();
-}
-
-// initialize server app
-const server = new Server();
-
-// make server listen on some port
-((port = process.env.APP_PORT || 5000) => {
-  server.app.listen(
-    port,
-    async () => {
-      // initialize the whole application
-      await initService.start();
-
-      // here we go baby, let them now!!
-      console.log(' ▄▄ •  ▄▄▄· ▄▄▄  ▄▄▄  ▪  .▄▄ ·        ▐ ▄ ');
-      console.log('▐█ ▀ ▪▐█ ▀█ ▀▄ █·▀▄ █·██ ▐█ ▀. ▪     •█▌▐█');
-      console.log('▄█ ▀█▄▄█▀▀█ ▐▀▀▄ ▐▀▀▄ ▐█·▄▀▀▀█▄ ▄█▀▄ ▐█▐▐▌');
-      console.log('▐█▄▪▐█▐█ ▪▐▌▐█•█▌▐█•█▌▐█▌▐█▄▪▐█▐█▌.▐▌██▐█▌');
-      console.log('·▀▀▀▀  ▀  ▀ .▀  ▀.▀  ▀▀▀▀ ▀▀▀▀  ▀█▄▀▪▀▀ █▪');
-      console.log(`> Version: v${require('../package')?.version}`);
-      console.log(`> Port: ${port}`);
-      console.log('> Ready to handle requests!\n');
+(async () => {
+  // load the environment variables from the .env file
+  dotenv.config({
+    path: '.env'
   });
+  
+  /**
+   * Express server application class.
+   * @description Will later contain the routing system.
+   */
+  class Server {
+    public app = express();
+    public master = <MasterRouter>{};
+
+    async configureRouter() {
+      await initService.start();
+      this.master = new MasterRouter(initService.ctService);
+    }
+  }
+
+  // initialize server app
+  const server = new Server();
+  await server.configureRouter();
+
+  // make server app handle any route starting with '/api'
+  server.app.use('/api', server.master.router);
+
+  // make server app handle any error
+  server.app.use((err: ErrorHandler, req: Request, res: Response, next: NextFunction) => {
+    console.error(err);
+    res.status(err.statusCode || 500).json({
+      status: 'error',
+      statusCode: err.statusCode,
+      message: err.message
+    });
+  });
+
+  // make server listen on some port
+  ((port = process.env.APP_PORT || 5000) => {
+    server.app.listen(
+      port,
+      () => {
+        // here we go baby, let them now!!
+        console.log(' ▄▄ •  ▄▄▄· ▄▄▄  ▄▄▄  ▪  .▄▄ ·        ▐ ▄ ');
+        console.log('▐█ ▀ ▪▐█ ▀█ ▀▄ █·▀▄ █·██ ▐█ ▀. ▪     •█▌▐█');
+        console.log('▄█ ▀█▄▄█▀▀█ ▐▀▀▄ ▐▀▀▄ ▐█·▄▀▀▀█▄ ▄█▀▄ ▐█▐▐▌');
+        console.log('▐█▄▪▐█▐█ ▪▐▌▐█•█▌▐█•█▌▐█▌▐█▄▪▐█▐█▌.▐▌██▐█▌');
+        console.log('·▀▀▀▀  ▀  ▀ .▀  ▀.▀  ▀▀▀▀ ▀▀▀▀  ▀█▄▀▪▀▀ █▪');
+        console.log(`> Version: v${require('../package')?.version}`);
+        console.log(`> Port: ${port}`);
+        console.log('> Ready to handle requests!\n');
+    });
+  })();
 })();

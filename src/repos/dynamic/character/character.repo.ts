bf5933b90e2556389b48b1719760a164edf500ae
Pierre Viara <viara@intechinfo.fr>
2021-03-09 11:25:59 +0100

    refactor(repo): 🏭 give character repository some doc and improve methods

--- src/repos/dynamic/character/character.repo.ts
+++ src/repos/dynamic/character/character.repo.ts
@@ -1,17 +1,26 @@
 import ErrorHandler from '../../../config/models/error/error-handler.model';
 
-import { ELogType as logType } from '../../../config/models/log/log.model';
+import {
+  ELogType as logType
+} from '../../../config/models/log/log.model';
 import IMonitored from '../../../config/models/IMonitored';
 import MonitoringService from '../../../config/services/monitoring/monitoring.service';
 
-import { ObjectId } from 'mongodb';
+import {
+  ObjectId
+} from 'mongodb';
 
-import { ICharacterModel } from '../../../config/models/data/dynamic/character/character.types';
+import {
+  ICharacterDocument,
+  ICharacterModel
+} from '../../../config/models/data/dynamic/character/character.types';
 import ICharacterCreate from '../../../config/models/data/dynamic/character/payloads/ICharacterCreate';
 
 import BannerRepository from '../../static/banner.repo';
 import FactionRepository from '../../static/faction.repo';
 import UserRepository from '../user/user.repo';
+
+import helper from '../../../utils/helper.utils';
 
 /**
  * Handle interactions with character documents from database dynamic.
@@ -22,7 +31,7 @@ export default class CharacterRepository implements IMonitored {
   get monitor() {
     return this._monitor;
   }
-  
+
   constructor(
     private _model: ICharacterModel,
     private _bannerRepo: BannerRepository,
@@ -32,30 +41,56 @@ export default class CharacterRepository implements IMonitored {
     this._monitor.log(logType.pass, 'Initialized character repository');
   }
 
-  async findById(id: ObjectId) {
-    return await this._model.findById(id);
+  /**
+   * Find a character by its id.
+   * @param id Given ObjectId.
+   * @param strict Sets whether an error is thrown when no character is found.
+   * @returns Either an ICharacterDocument or (maybe) null if strict mode is set to false.
+   */
+  async findById(id: ObjectId, strict ? : true): Promise < ICharacterDocument > ;
+  async findById(id: ObjectId, strict: false): Promise < ICharacterDocument | null > ;
+  async findById(id: ObjectId, strict ? : boolean) {
+    const result = await this._model.findById(id);
+    if (!result && strict) throw new ErrorHandler(404, `Character with characterId ${id} couldn't be found.`);
+
+    return result;
   }
 
-  async getFromUser(userId: ObjectId) {
-    return await this._model.findOne({ userId });
+  /**
+   * Get a character from a user's id.
+   * @param userId Given ObjectId.
+   * @param strict Sets whether an error is thrown when no character is found.
+   * @returns Either an ICharacterDocument or (maybe) null if strict mode is set to false.
+   */
+  async getFromUser(userId: ObjectId, strict ? : true): Promise < ICharacterDocument > ;
+  async getFromUser(userId: ObjectId, strict: false): Promise < ICharacterDocument | null > ;
+  async getFromUser(userId: ObjectId, strict = true) {
+    const result = await this._model.findOne({
+      userId
+    });
+    if (!result && strict) throw new ErrorHandler(404, `Character from userId ${userId} couldn't be found.`);
+
+    return result;
   }
 
+  /**
+   * Create and save a new character in database.
+   * @param payload @see ICharacterCreate
+   */
   async create(payload: ICharacterCreate) {
-    const userCharacter = await this.getFromUser(payload.userId);
-    const existing = userCharacter?.name.toLowerCase() === payload.name.toLowerCase();
-    if (existing) throw new ErrorHandler(409, 'Already existing character.');
+    const userCharacter = await this.getFromUser(payload.userId, false);
+    if (userCharacter && helper.areSameString(userCharacter.name, payload.name)) {
+      throw new ErrorHandler(409, `Already existing character with name '${payload.name}'.`);
+    }
 
     // check on user existence
-    const user = await this._userRepo.findById(payload.userId);
-    if (!user) throw new ErrorHandler(404, 'User couldn\'t be found.');
+    await this._userRepo.findById(payload.userId);
 
     // check on faction existence
-    const faction = await this._factionRepo.findByCode(payload.side.faction);
-    if (!faction) throw new ErrorHandler(404, 'Faction couldn\'t be found.');
+    await this._factionRepo.findByCode(payload.side.faction);
 
     // check on banner existence
-    const banner = await this._bannerRepo.findByCode(payload.side.banner);
-    if (!banner) throw new ErrorHandler(404, 'Banner couldn\'t be found.');
+    await this._bannerRepo.findByCode(payload.side.banner);
 
     // create character
     return this._model.create({
